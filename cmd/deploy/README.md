fdb322af89c54ecef7c8febb8dfb8ddb9d7bd1a6
nkbai <nkbai@163.com>
2018-01-23 14:56:34 +0800

    deploy

---
+++ cmd/deploy/README.md
@@ -0,0 +1,3 @@
+# deploy
+
+deploy is  a tool to help deploy all raiden contract to ethereum.
---
+++ cmd/deploy/main.go
@@ -0,0 +1,166 @@
+package main
+
+import (
+	"log"
+	"strings"
+
+	"context"
+
+	"fmt"
+
+	"os"
+
+	"crypto/ecdsa"
+
+	"github.com/SmartMeshFoundation/raiden-network"
+	"github.com/SmartMeshFoundation/raiden-network/abi/bind"
+	"github.com/SmartMeshFoundation/raiden-network/network/rpc"
+	"github.com/SmartMeshFoundation/raiden-network/params"
+	"github.com/SmartMeshFoundation/raiden-network/utils"
+	ethutils "github.com/ethereum/go-ethereum/cmd/utils"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/ethereum/go-ethereum/node"
+	"github.com/slonzok/getpass"
+	"gopkg.in/urfave/cli.v1"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Flags = []cli.Flag{
+		cli.StringFlag{
+			Name:  "address",
+			Usage: "The ethereum address you would like raiden to use and for which a keystore file exists in your local system.",
+		},
+		ethutils.DirectoryFlag{
+			Name:  "keystore-path",
+			Usage: "If you have a non-standard path for the ethereum keystore directory provide it using this argument. ",
+			Value: ethutils.DirectoryString{params.DefaultKeyStoreDir()},
+		},
+		cli.StringFlag{
+			Name: "eth-rpc-endpoint",
+			Usage: `"host:port" address of ethereum JSON-RPC server.\n'
+	           'Also accepts a protocol prefix (ws:// or ipc channel) with optional port',`,
+			Value: node.DefaultIPCEndpoint("geth"),
+		},
+	}
+	app.Action = Main
+	app.Name = "raidendeploy"
+	app.Version = "0.1"
+	app.Run(os.Args)
+}
+
+func Main(ctx *cli.Context) error {
+	// Create an IPC based RPC connection to a remote node and an authorized transactor
+	conn, err := ethclient.Dial(ctx.String("eth-rpc-endpoint"))
+	if err != nil {
+		log.Fatalf("Failed to connect to the Ethereum client: %v", err)
+	}
+	address := common.HexToAddress(ctx.String("address"))
+	address, key := promptAccount(address, ctx.String("keystore-path"))
+	fmt.Println("start to deploy ...")
+	deployContract(key, conn)
+	return nil
+}
+func deployContract(key *ecdsa.PrivateKey, conn *ethclient.Client) {
+	auth := bind.NewKeyedTransactor(key)
+	//DeployNettingChannelLibrary
+	NettingChannelLibraryAddress, tx, _, err := rpc.DeployNettingChannelLibrary(auth, conn)
+	if err != nil {
+		log.Fatalf("Failed to deploy new token contract: %v", err)
+	}
+	ctx := context.Background()
+	_, err = bind.WaitDeployed(ctx, conn, tx)
+	if err != nil {
+		log.Fatalf("failed to deploy contact when mining :%v", err)
+	}
+	fmt.Printf("DeployNettingChannelLibrary complete...\n")
+	//DeployChannelManagerLibrary link nettingchannle library before deploy
+	rpc.ChannelManagerLibraryBin = strings.Replace(rpc.ChannelManagerLibraryBin, "__NettingChannelLibrary.sol:NettingCha__", NettingChannelLibraryAddress.String()[2:], -1)
+	ChannelManagerLibraryAddress, tx, _, err := rpc.DeployChannelManagerLibrary(auth, conn)
+	if err != nil {
+		log.Fatalf("Failed to deploy new token contract: %v", err)
+	}
+	ctx = context.Background()
+	_, err = bind.WaitDeployed(ctx, conn, tx)
+	if err != nil {
+		log.Fatalf("failed to deploy contact when mining :%v", err)
+	}
+	fmt.Printf("DeployChannelManagerLibrary complete...\n")
+	//DeployRegistry link channelmanagerlibrary before deploy
+	rpc.RegistryBin = strings.Replace(rpc.RegistryBin, "__ChannelManagerLibrary.sol:ChannelMan__", ChannelManagerLibraryAddress.String()[2:], -1)
+	RegistryAddress, tx, _, err := rpc.DeployRegistry(auth, conn)
+	if err != nil {
+		log.Fatalf("Failed to deploy new token contract: %v", err)
+	}
+	ctx = context.Background()
+	_, err = bind.WaitDeployed(ctx, conn, tx)
+	if err != nil {
+		log.Fatalf("failed to deploy contact when mining :%v", err)
+	}
+	fmt.Printf("DeployRegistry complete...\n")
+	//DeployEndpointRegistry
+	EndpointRegistryAddress, tx, _, err := rpc.DeployEndpointRegistry(auth, conn)
+	if err != nil {
+		log.Fatalf("Failed to deploy new token contract: %v", err)
+	}
+	ctx = context.Background()
+	_, err = bind.WaitDeployed(ctx, conn, tx)
+	if err != nil {
+		log.Fatalf("failed to deploy contact when mining :%v", err)
+	}
+	fmt.Printf("RegistryAddress=%s\nEndpointRegistryAddress=%s\n", RegistryAddress.String(), EndpointRegistryAddress.String())
+	//RegistryAddress=0x1026a4441921EcF88aaF13014d96aF90f735a02c
+	//EndpointRegistryAddress=0xB85b8b57e2b701d5E918D7d9027A7330472a663a
+}
+func promptAccount(adviceAddress common.Address, keystorePath string) (addr common.Address, key *ecdsa.PrivateKey) {
+	am := raiden_network.NewAccountManager(keystorePath)
+	if len(am.Accounts) == 0 {
+		log.Fatal(fmt.Sprintf("No Ethereum accounts found in the directory %s", keystorePath))
+		os.Exit(1)
+	}
+	if !am.AddressInKeyStore(adviceAddress) {
+		if adviceAddress != utils.EmptyAddress {
+			log.Fatal(fmt.Sprintf("account %s could not be found on the sytstem. aborting...", adviceAddress))
+			os.Exit(1)
+		}
+		shouldPromt := true
+		fmt.Println("The following accounts were found in your machine:")
+		for i := 0; i < len(am.Accounts); i++ {
+			fmt.Printf("%3d -  %s\n", i, am.Accounts[i].Address.String())
+		}
+		fmt.Println("")
+		for shouldPromt {
+			fmt.Printf("Select one of them by index to continue:")
+			idx := -1
+			fmt.Scanf("%d\n", &idx)
+			if idx >= 0 && idx < len(am.Accounts) {
+				shouldPromt = false
+				addr = am.Accounts[idx].Address
+			} else {
+				fmt.Printf("Error: Provided index %d is out of bounds", idx)
+			}
+		}
+	} else {
+		addr = adviceAddress
+	}
+	var password string
+	for i := 0; i < 3; i++ {
+		//retries three times
+		password = getpass.Prompt("Enter the password to unlock")
+		//fmt.Printf("\npassword is %s\n", password)
+		keybin, err := am.GetPrivateKey(addr, password)
+		if err != nil && i == 3 {
+			log.Fatal(fmt.Sprintf("Exhausted passphrase unlock attempts for %s. Aborting ...", addr))
+			os.Exit(1)
+		}
+		if err != nil {
+			log.Println(fmt.Sprintf("password incorrect\n Please try again or kill the process to quit.\nUsually Ctrl-c."))
+			continue
+		}
+		key, _ = crypto.ToECDSA(keybin)
+		break
+	}
+	return
+}
