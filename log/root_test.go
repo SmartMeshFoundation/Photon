48106a5637bbab9edd2c8ba7bb0d92a5b3619cc3
bai <nkbai@163.com>
2018-05-04 23:39:55 +0800

    log display millisecond

--- log/root_test.go
+++ log/root_test.go
@@ -7,4 +7,8 @@ func TestDebug(t *testing.T) {
 	root.Info("stdout handler")
 	root.SetHandler(StderrHandler)
 	root.Info("stdinfo handler")
+	l := New("id", 30, "conn", "ok")
+	l.Info("from id")
+	l2 := l.New("submodule", "test")
+	l2.Info("from l2")
 }
