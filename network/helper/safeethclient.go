69153120a720c673da60ddc7fd1cd77928812855
bai <nkbai@163.com>
2018-05-21 10:55:22 +0800

    golint helper

--- network/helper/safeethclient.go
+++ network/helper/safeethclient.go
@@ -14,10 +14,13 @@ import (
 	"github.com/fatedier/frp/src/utils/log"
 )
 
+//ConnectionStatus is status of connection to ethereum
 type ConnectionStatus int
 
 const (
-	ConnectionOk     ConnectionStatus = 0
+	//ConnectionOk connection status is success
+	ConnectionOk ConnectionStatus = 0
+	//ConnectionFailed connection now is disconnected
 	ConnectionFailed ConnectionStatus = 1
 )
 
@@ -30,6 +33,7 @@ type SafeEthClient struct {
 	Status    ConnectionStatus
 }
 
+//NewSafeClient create safeclient
 func NewSafeClient(rawurl string) (*SafeEthClient, error) {
 	c := new(SafeEthClient)
 	c.ReConnect = make(map[string]chan struct{})
@@ -43,218 +47,246 @@ func NewSafeClient(rawurl string) (*SafeEthClient, error) {
 	}
 	return c, err
 }
-func (this *SafeEthClient) RegisterReConnectNotify(name string) <-chan struct{} {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	c, ok := this.ReConnect[name]
+
+//RegisterReConnectNotify register notify when reconnect
+func (c *SafeEthClient) RegisterReConnectNotify(name string) <-chan struct{} {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	ch, ok := c.ReConnect[name]
 	if ok {
 		log.Warn("NeedReConnectNotify should only call once")
-		return c
+		return ch
 	}
-	c = make(chan struct{}, 1)
-	this.ReConnect[name] = c
-	return c
+	ch = make(chan struct{}, 1)
+	c.ReConnect[name] = ch
+	return ch
 }
 
-//try to reconnect with geth after a restart of geth
-func (this *SafeEthClient) RecoverDisconnect() {
+//RecoverDisconnect try to reconnect with geth after a restart of geth
+func (c *SafeEthClient) RecoverDisconnect() {
 	var err error
 	var client *ethclient.Client
-	this.Status = ConnectionFailed
+	c.Status = ConnectionFailed
 	for {
 		log.Info("tyring to reconnect geth ...")
-		client, err = ethclient.Dial(this.url)
+		client, err = ethclient.Dial(c.url)
 		if err != nil {
 			log.Info("reconnect to geth error:", err)
 		} else {
 			//reconnect ok
-			this.Status = ConnectionOk
-			this.lock.Lock()
-			this.Client = client
+			c.Status = ConnectionOk
+			c.lock.Lock()
+			c.Client = client
 			var keys []string
-			for name, c := range this.ReConnect {
+			for name, c := range c.ReConnect {
 				keys = append(keys, name)
 				c <- struct{}{}
 				close(c)
 			}
 			for _, name := range keys {
-				delete(this.ReConnect, name)
+				delete(c.ReConnect, name)
 			}
-			this.lock.Unlock()
+			c.lock.Unlock()
 			return
 		}
 		time.Sleep(time.Second * 3)
 	}
 }
-func (this *SafeEthClient) BlockByHash(ctx context.Context, hash common.Hash) (r1 *types.Block, err error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	r1, err = this.Client.BlockByHash(ctx, hash)
+
+//BlockByHash wrapper of BlockByHash
+func (c *SafeEthClient) BlockByHash(ctx context.Context, hash common.Hash) (r1 *types.Block, err error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	r1, err = c.Client.BlockByHash(ctx, hash)
 	return
 }
 
-func (this *SafeEthClient) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.BlockByNumber(ctx, number)
+//BlockByNumber wrapper of BlockByNumber
+func (c *SafeEthClient) BlockByNumber(ctx context.Context, number *big.Int) (*types.Block, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.BlockByNumber(ctx, number)
 }
 
 // HeaderByHash returns the block header with the given hash.
-func (this *SafeEthClient) HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.HeaderByHash(ctx, hash)
+func (c *SafeEthClient) HeaderByHash(ctx context.Context, hash common.Hash) (*types.Header, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.HeaderByHash(ctx, hash)
 }
 
 // HeaderByNumber returns a block header from the current canonical chain. If number is
 // nil, the latest known header is returned.
-func (this *SafeEthClient) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.HeaderByNumber(ctx, number)
+func (c *SafeEthClient) HeaderByNumber(ctx context.Context, number *big.Int) (*types.Header, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.HeaderByNumber(ctx, number)
 }
 
-func (this *SafeEthClient) TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, isPending bool, err error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.TransactionByHash(ctx, hash)
+//TransactionByHash wrapper of TransactionByHash
+func (c *SafeEthClient) TransactionByHash(ctx context.Context, hash common.Hash) (tx *types.Transaction, isPending bool, err error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.TransactionByHash(ctx, hash)
 }
 
-func (this *SafeEthClient) TransactionSender(ctx context.Context, tx *types.Transaction, block common.Hash, index uint) (common.Address, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.TransactionSender(ctx, tx, block, index)
+//TransactionSender wrapper of TransactionSender
+func (c *SafeEthClient) TransactionSender(ctx context.Context, tx *types.Transaction, block common.Hash, index uint) (common.Address, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.TransactionSender(ctx, tx, block, index)
 }
 
 // TransactionCount returns the total number of transactions in the given block.
-func (this *SafeEthClient) TransactionCount(ctx context.Context, blockHash common.Hash) (uint, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.TransactionCount(ctx, blockHash)
+func (c *SafeEthClient) TransactionCount(ctx context.Context, blockHash common.Hash) (uint, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.TransactionCount(ctx, blockHash)
 }
 
-func (this *SafeEthClient) TransactionInBlock(ctx context.Context, blockHash common.Hash, index uint) (*types.Transaction, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.TransactionInBlock(ctx, blockHash, index)
+//TransactionInBlock wrapper of TransactionInBlock
+func (c *SafeEthClient) TransactionInBlock(ctx context.Context, blockHash common.Hash, index uint) (*types.Transaction, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.TransactionInBlock(ctx, blockHash, index)
 }
 
-func (this *SafeEthClient) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.TransactionReceipt(ctx, txHash)
+//TransactionReceipt wrappper of TransactionReceipt
+func (c *SafeEthClient) TransactionReceipt(ctx context.Context, txHash common.Hash) (*types.Receipt, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.TransactionReceipt(ctx, txHash)
 }
 
-func (this *SafeEthClient) SyncProgress(ctx context.Context) (*ethereum.SyncProgress, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.SyncProgress(ctx)
+//SyncProgress wrapper of SyncProgress
+func (c *SafeEthClient) SyncProgress(ctx context.Context) (*ethereum.SyncProgress, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.SyncProgress(ctx)
 }
 
-func (this *SafeEthClient) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.SubscribeNewHead(ctx, ch)
+//SubscribeNewHead wrapper of SubscribeNewHead
+func (c *SafeEthClient) SubscribeNewHead(ctx context.Context, ch chan<- *types.Header) (ethereum.Subscription, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.SubscribeNewHead(ctx, ch)
 }
 
-func (this *SafeEthClient) NetworkID(ctx context.Context) (*big.Int, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.NetworkID(ctx)
+//NetworkID wrapper of NetworkID
+func (c *SafeEthClient) NetworkID(ctx context.Context) (*big.Int, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.NetworkID(ctx)
 }
 
-func (this *SafeEthClient) BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.BalanceAt(ctx, account, blockNumber)
+//BalanceAt wrapper of BalanceAt
+func (c *SafeEthClient) BalanceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (*big.Int, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.BalanceAt(ctx, account, blockNumber)
 }
 
-func (this *SafeEthClient) StorageAt(ctx context.Context, account common.Address, key common.Hash, blockNumber *big.Int) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.StorageAt(ctx, account, key, blockNumber)
-}
-func (this *SafeEthClient) CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.CodeAt(ctx, account, blockNumber)
+//StorageAt wrapper of StorageAt
+func (c *SafeEthClient) StorageAt(ctx context.Context, account common.Address, key common.Hash, blockNumber *big.Int) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.StorageAt(ctx, account, key, blockNumber)
 }
 
-func (this *SafeEthClient) NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.NonceAt(ctx, account, blockNumber)
+//CodeAt wrapper of CodeAt
+func (c *SafeEthClient) CodeAt(ctx context.Context, account common.Address, blockNumber *big.Int) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.CodeAt(ctx, account, blockNumber)
 }
 
-func (this *SafeEthClient) FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.FilterLogs(ctx, q)
+//NonceAt wrapper of NonceAt
+func (c *SafeEthClient) NonceAt(ctx context.Context, account common.Address, blockNumber *big.Int) (uint64, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.NonceAt(ctx, account, blockNumber)
 }
 
-func (this *SafeEthClient) SubscribeFilterLogs(ctx context.Context, q ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.SubscribeFilterLogs(ctx, q, ch)
+//FilterLogs wrapper of FilterLogs
+func (c *SafeEthClient) FilterLogs(ctx context.Context, q ethereum.FilterQuery) ([]types.Log, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.FilterLogs(ctx, q)
 }
 
-func (this *SafeEthClient) PendingBalanceAt(ctx context.Context, account common.Address) (*big.Int, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingBalanceAt(ctx, account)
+//SubscribeFilterLogs wrapper of SubscribeFilterLogs
+func (c *SafeEthClient) SubscribeFilterLogs(ctx context.Context, q ethereum.FilterQuery, ch chan<- types.Log) (ethereum.Subscription, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.SubscribeFilterLogs(ctx, q, ch)
 }
 
-func (this *SafeEthClient) PendingStorageAt(ctx context.Context, account common.Address, key common.Hash) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingStorageAt(ctx, account, key)
+//PendingBalanceAt wrapper of PendingBalanceAt
+func (c *SafeEthClient) PendingBalanceAt(ctx context.Context, account common.Address) (*big.Int, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingBalanceAt(ctx, account)
 }
 
-func (this *SafeEthClient) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingCodeAt(ctx, account)
+//PendingStorageAt wrapper of PendingStorageAt
+func (c *SafeEthClient) PendingStorageAt(ctx context.Context, account common.Address, key common.Hash) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingStorageAt(ctx, account, key)
 }
 
-func (this *SafeEthClient) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingNonceAt(ctx, account)
+//PendingCodeAt wrapper of PendingCodeAt
+func (c *SafeEthClient) PendingCodeAt(ctx context.Context, account common.Address) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingCodeAt(ctx, account)
+}
+
+//PendingNonceAt wrapper of PendingNonceAt
+func (c *SafeEthClient) PendingNonceAt(ctx context.Context, account common.Address) (uint64, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingNonceAt(ctx, account)
 }
 
 // PendingTransactionCount returns the total number of transactions in the pending state.
-func (this *SafeEthClient) PendingTransactionCount(ctx context.Context) (uint, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingTransactionCount(ctx)
+func (c *SafeEthClient) PendingTransactionCount(ctx context.Context) (uint, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingTransactionCount(ctx)
 }
 
-func (this *SafeEthClient) CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.CallContract(ctx, msg, blockNumber)
+//CallContract wrapper of CallContract
+func (c *SafeEthClient) CallContract(ctx context.Context, msg ethereum.CallMsg, blockNumber *big.Int) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.CallContract(ctx, msg, blockNumber)
 }
 
-func (this *SafeEthClient) PendingCallContract(ctx context.Context, msg ethereum.CallMsg) ([]byte, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.PendingCallContract(ctx, msg)
+//PendingCallContract wrapper of PendingCallContract
+func (c *SafeEthClient) PendingCallContract(ctx context.Context, msg ethereum.CallMsg) ([]byte, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.PendingCallContract(ctx, msg)
 }
 
-func (this *SafeEthClient) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.SuggestGasPrice(ctx)
+//SuggestGasPrice wrapper of SuggestGasPrice
+func (c *SafeEthClient) SuggestGasPrice(ctx context.Context) (*big.Int, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.SuggestGasPrice(ctx)
 }
 
-func (this *SafeEthClient) EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error) {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.EstimateGas(ctx, msg)
+//EstimateGas wrapper of EstimateGas
+func (c *SafeEthClient) EstimateGas(ctx context.Context, msg ethereum.CallMsg) (uint64, error) {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.EstimateGas(ctx, msg)
 }
 
-func (this *SafeEthClient) SendTransaction(ctx context.Context, tx *types.Transaction) error {
-	this.lock.Lock()
-	defer this.lock.Unlock()
-	return this.Client.SendTransaction(ctx, tx)
+//SendTransaction wrapper of SendTransaction
+func (c *SafeEthClient) SendTransaction(ctx context.Context, tx *types.Transaction) error {
+	c.lock.Lock()
+	defer c.lock.Unlock()
+	return c.Client.SendTransaction(ctx, tx)
 }
