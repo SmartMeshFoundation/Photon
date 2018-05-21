790fb3cf84380a13dd3dd84e2d317b9b1e14c311
bai <nkbai@163.com>
2018-05-21 10:55:51 +0800

    golint fee

--- network/rpc/fee/feecharger.go
+++ network/rpc/fee/feecharger.go
@@ -6,7 +6,11 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
-type FeeCharger interface {
-	//returns how many tokens charge for transfer 'amount' tokens on token who's address is tokenAddress.
+/*
+Charger defines how to charge fee for a mediated node
+it relates which token and how much to transfer.
+*/
+type Charger interface {
+	//GetNodeChargeFee returns how many tokens charge for transfer 'amount' tokens on token who's address is tokenAddress.
 	GetNodeChargeFee(nodeAddress, tokenAddress common.Address, amount *big.Int) *big.Int
 }
