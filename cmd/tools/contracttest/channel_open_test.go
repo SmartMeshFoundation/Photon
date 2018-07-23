97dd8741bde8cfd1e9075858a90a11f12a01f5ea
senhuawang <123213441@qq.com>
2018-07-23 20:04:07 +0800

    contract test

--- cmd/tools/contracttest/channel_open_test.go
+++ cmd/tools/contracttest/channel_open_test.go
@@ -6,11 +6,12 @@ import (
 	"github.com/SmartMeshFoundation/SmartRaiden/network/rpc/contracts"
 	"github.com/ethereum/go-ethereum/common"
 	"github.com/stretchr/testify/assert"
+	"math/big"
 )
 
 // TestOpenChannelFail :
 func TestOpenChannelFail(t *testing.T) {
-	t.Log("Test channel open ...")
+	t.Log("Test channel open to fail ...")
 	InitEnv(t, "./env.INI")
 	a1, a2 := env.getTwoRandomAccount()
 	testSettleTimeout := TestSettleTimeoutMin + 5
@@ -51,11 +52,12 @@ func TestOpenChannelFail(t *testing.T) {
 	// test cases 12
 	_, err = env.TokenNetwork.OpenChannel(a1.Auth, a1.Address, a2.Address, TestSettleTimeoutMax+1)
 	assert.NotEmpty(t, err, err.Error())
+	t.Log("Test done SUCCESS")
 }
 
 // TestOpenChannelState :
 func TestOpenChannelState(t *testing.T) {
-	t.Log("Test open channel ...")
+	t.Log("Test open channel state ...")
 	InitEnv(t, "./env.INI")
 	a1, a2 := env.getTwoRandomAccount()
 	testSettleTimeout := TestSettleTimeoutMin + 10
@@ -69,8 +71,8 @@ func TestOpenChannelState(t *testing.T) {
 	// test cases 3
 	deposit, balanceHash, nonce, err := env.TokenNetwork.GetChannelParticipantInfo(nil, a1.Address, a2.Address)
 	assert.Empty(t, err)
-	assert.Equal(t, 0, deposit)
-	assert.Equal(t, 0, nonce)
+	assert.Equal(t, big.NewInt(0), *deposit)
+	assert.Equal(t, uint(0), nonce)
 	assert.Equal(t, nil, balanceHash)
 	// test cases 4
 	deposit, balanceHash, nonce, err = env.TokenNetwork.GetChannelParticipantInfo(nil, a2.Address, a1.Address)
@@ -78,6 +80,7 @@ func TestOpenChannelState(t *testing.T) {
 	assert.Equal(t, 0, deposit)
 	assert.Equal(t, 0, nonce)
 	assert.Equal(t, nil, balanceHash)
+	t.Log("Test done SUCCESS")
 }
 
 // TestOpenChannelRepeat :
@@ -95,4 +98,5 @@ func TestOpenChannelRepeat(t *testing.T) {
 	// test cases 2
 	_, err = env.TokenNetwork.OpenChannel(a1.Auth, a2.Address, a1.Address, testSettleTimeout)
 	assert.NotEmpty(t, err, err.Error())
+	t.Log("Test done SUCCESS")
 }
