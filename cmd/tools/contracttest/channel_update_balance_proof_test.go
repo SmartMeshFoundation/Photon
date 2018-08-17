8aa52606b707d0b0ae7f1e8ba29f784f9ebbb7a6
Chuck <wuhan_53@163.com>
2018-08-17 16:46:40 +0800

    contract test add cases about update balance proof

--- cmd/tools/contracttest/channel_update_balance_proof_test.go
+++ cmd/tools/contracttest/channel_update_balance_proof_test.go
@@ -1,11 +1,59 @@
 package contracttest
 
-import "testing"
+import (
+	"math/big"
+	"testing"
+
+	"github.com/SmartMeshFoundation/SmartRaiden/utils"
+	"github.com/ethereum/go-ethereum/common"
+)
 
 // TestUpdateBalanceProofRight : 正确调用测试
 func TestUpdateBalanceProofRight(t *testing.T) {
 	InitEnv(t, "./env.INI")
 	count := 0
+	// prepare
+	testSettleTimeout := TestSettleTimeoutMin + 1
+	self, partner := env.getTwoAccountWithoutChannelClose(t)
+	// open channel
+	cooperativeSettleChannelIfExists(self, partner)
+	openChannelAndDeposit(self, partner, big.NewInt(15), big.NewInt(10), testSettleTimeout)
+	// partner close channel
+	tx, err := env.TokenNetwork.CloseChannel(partner.Auth, self.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxSuccess(t, nil, tx, err)
+	// create balance proof
+	bpPartner := createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
+
+	// update right, MUST SUCCESS
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxSuccess(t, &count, tx, err)
+
+	// continue update right, MUST SUCCESS
+	bpPartner.Nonce = bpPartner.Nonce + 1
+	bpPartner.TransferAmount = bpPartner.TransferAmount.Add(bpPartner.TransferAmount, big.NewInt(10))
+	bpPartner.sign(partner.Key)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxSuccess(t, &count, tx, err)
+
+	// check self state after update
+	depositSelf, balanceHashSelf, nonceSelf, err := env.TokenNetwork.GetChannelParticipantInfo(nil, self.Address, partner.Address)
+	assertSuccess(t, nil, err)
+	assertEqual(t, &count, big.NewInt(15), depositSelf)
+	assertEqual(t, &count, utils.EmptyHash[:24], balanceHashSelf[:])
+	assertEqual(t, &count, uint64(0), nonceSelf)
+
+	// check partner state after update
+	depositPartner, balanceHashPartner, noncePartner, err := env.TokenNetwork.GetChannelParticipantInfo(nil, partner.Address, self.Address)
+	localBpPartnerBalanceHash := bpPartner.BalanceData.Hash()
+	assertSuccess(t, nil, err)
+	assertEqual(t, &count, big.NewInt(10), depositPartner)
+	assertEqual(t, &count, localBpPartnerBalanceHash[:24], balanceHashPartner[:])
+	assertEqual(t, &count, bpPartner.Nonce, noncePartner)
+
+	// settle
+	waitForSettle(testSettleTimeout)
+	tx, err = env.TokenNetwork.SettleChannel(self.Auth, self.Address, big.NewInt(0), utils.EmptyHash, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot)
+	assertTxSuccess(t, nil, tx, err)
 	t.Log(endMsg("UpdateBalanceProof 正确调用测试", count))
 }
 
@@ -13,14 +61,99 @@ func TestUpdateBalanceProofRight(t *testing.T) {
 func TestUpdateBalanceProofException(t *testing.T) {
 	InitEnv(t, "./env.INI")
 	count := 0
-	t.Log(endMsg("UpdateBalanceProof 异常调用测试", count))
+	// prepare
+	testSettleTimeout := TestSettleTimeoutMin + 1
+	self, partner := env.getTwoAccountWithoutChannelClose(t)
+	// create balance proof with nonexistent channel
+	bpPartnerFake := createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
+	// update balance proof on nonexistent channel, MUST FAIL
+	tx, err := env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartnerFake.TransferAmount, bpPartnerFake.LocksRoot, bpPartnerFake.Nonce, bpPartnerFake.AdditionalHash, bpPartnerFake.Signature)
+	assertTxFail(t, &count, tx, err)
+	// create channel
+	openChannelAndDeposit(self, partner, big.NewInt(25), big.NewInt(0), testSettleTimeout)
+	// create balance proof
+	bpPartner := createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
 
+	// update balance proof on open channel, MUST FAIL
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+
+	// partner close channel
+	tx, err = env.TokenNetwork.CloseChannel(partner.Auth, self.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxSuccess(t, nil, tx, err)
+
+	// update with no off-chain transfer, MUST FAIL
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxFail(t, &count, tx, err)
+	bpPartnerEmpty := createPartnerBalanceProof(self, partner, big.NewInt(0), utils.EmptyHash, utils.EmptyHash, 0)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartnerEmpty.TransferAmount, bpPartnerEmpty.LocksRoot, bpPartnerEmpty.Nonce, bpPartnerEmpty.AdditionalHash, bpPartnerEmpty.Signature)
+	assertTxFail(t, &count, tx, err)
+
+	// right update, MUST SUCCESS
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxSuccess(t, &count, tx, err)
+
+	// update with same nonce, MUST FAIL
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+
+	// update with lower nonce, MUST FAIL
+	bpPartner.Nonce = bpPartner.Nonce - 1
+	bpPartner.sign(partner.Key)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+
+	// after settle timeout, MUST FAIL
+	waitForSettle(testSettleTimeout)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+
+	// settle
+	tx, err = env.TokenNetwork.SettleChannel(self.Auth, self.Address, big.NewInt(0), utils.EmptyHash, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot)
+	assertTxSuccess(t, nil, tx, err)
+	t.Log(endMsg("UpdateBalanceProof 异常调用测试", count))
 }
 
 // TestUpdateBalanceProofEdge : 边界测试
 func TestUpdateBalanceProofEdge(t *testing.T) {
 	InitEnv(t, "./env.INI")
 	count := 0
+	// prepare
+	testSettleTimeout := TestSettleTimeoutMin + 1
+	self, partner := env.getTwoAccountWithoutChannelClose(t)
+	// open channel
+	cooperativeSettleChannelIfExists(self, partner)
+	openChannelAndDeposit(self, partner, big.NewInt(15), big.NewInt(10), testSettleTimeout)
+	// partner close channel
+	tx, err := env.TokenNetwork.CloseChannel(partner.Auth, self.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxSuccess(t, nil, tx, err)
+	// create balance proof
+	bpPartner := createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
+	// self update proof with wrong parm
+	// EmptyAddress
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, EmptyAccountAddress, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// wrong transfer amount
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, big.NewInt(1), bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// wrong locksroot
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, common.HexToHash("0x123"), bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// wrong nonce
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, 3, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// wrong additional hash
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, common.HexToHash("0x123"), nil)
+	assertTxFail(t, &count, tx, err)
+	// wrong signature
+	bpPartner.sign(self.Key)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// wait for settle
+	waitForSettle(testSettleTimeout)
+	// settle
+	tx, err = env.TokenNetwork.SettleChannel(self.Auth, self.Address, big.NewInt(0), utils.EmptyHash, partner.Address, big.NewInt(0), utils.EmptyHash)
+	assertTxSuccess(t, nil, tx, err)
 	t.Log(endMsg("UpdateBalanceProof 边界测试", count))
 }
 
@@ -28,5 +161,36 @@ func TestUpdateBalanceProofEdge(t *testing.T) {
 func TestUpdateBalanceProofAttack(t *testing.T) {
 	InitEnv(t, "./env.INI")
 	count := 0
+	// prepare
+	testSettleTimeout := TestSettleTimeoutMin + 1
+	self, partner := env.getTwoAccountWithoutChannelClose(t)
+	// open channel
+	cooperativeSettleChannelIfExists(self, partner)
+	openChannelAndDeposit(self, partner, big.NewInt(15), big.NewInt(10), testSettleTimeout)
+	// partner close channel
+	tx, err := env.TokenNetwork.CloseChannel(partner.Auth, self.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxSuccess(t, nil, tx, err)
+	// create balance proof
+	bpPartner := createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
+	// update right, MUST SUCCESS
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxSuccess(t, &count, tx, err)
+	// settle
+	waitForSettle(testSettleTimeout)
+	tx, err = env.TokenNetwork.SettleChannel(self.Auth, self.Address, big.NewInt(0), utils.EmptyHash, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot)
+	assertTxSuccess(t, nil, tx, err)
+
+	// reopen and update with old balance proof, MUST FAIL
+	openChannelAndDeposit(self, partner, big.NewInt(15), big.NewInt(10), testSettleTimeout)
+	tx, err = env.TokenNetwork.CloseChannel(partner.Auth, self.Address, big.NewInt(0), utils.EmptyHash, 0, utils.EmptyHash, nil)
+	assertTxSuccess(t, nil, tx, err)
+	// update with old balance proof, MUST FAIL
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxFail(t, &count, tx, err)
+	// update with new balance proof, MUST SUCCESS
+	bpPartner = createPartnerBalanceProof(self, partner, big.NewInt(10), utils.EmptyHash, utils.EmptyHash, 5)
+	tx, err = env.TokenNetwork.UpdateBalanceProof(self.Auth, partner.Address, bpPartner.TransferAmount, bpPartner.LocksRoot, bpPartner.Nonce, bpPartner.AdditionalHash, bpPartner.Signature)
+	assertTxSuccess(t, &count, tx, err)
+
 	t.Log(endMsg("UpdateBalanceProof 恶意调用测试", count))
 }
