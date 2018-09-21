582f5a3d0bf4fdd45d05dce0f85bfe0396a2550d
calvindc <466862683@qq.com>
2018-09-21 15:34:23 +0800

    test discoveryroom

--- params/setttings.go
+++ params/setttings.go
@@ -119,8 +119,7 @@ var ChainID = big.NewInt(1)
 var MatrixServerConfig = [][]string{
 	//{"https://transport01.raiden.network", "transport01.raiden.network"},
 	//{"https://transport02.raiden.network", "transport02.raiden.network"},
-	//{"http://transport01.smartraiden.network:8008", "transport01.smartraiden.network"},
-	{"http://transport01.smartmesh.cn:8008", "transport01.smartmesh.cn"},
+	//{"http://transport01.smartmesh.cn:8008", "transport01.smartmesh.cn"},
 	{"http://transport02.smartmesh.cn:8008", "transport02.smartmesh.cn"},
 	{"http://transport03.smartmesh.cn:8008", "transport03.smartmesh.cn"},
 }
@@ -128,7 +127,8 @@ var MatrixServerConfig = [][]string{
 //MatrixDiscoveryRoomConfig a defalut matrix room config
 var MatrixDiscoveryRoomConfig = [][]string{
 	{"aliassegment", "discovery"},
-	{"server", "transport01.smartmesh.cn"},
+	{"server", "transport02.smartmesh.cn"},
+	//{"server", "transport01.raiden.network"},
 }
 
 //NETWORKNAME Specify the network name of the Ethereum network to run SmartRaiden on
