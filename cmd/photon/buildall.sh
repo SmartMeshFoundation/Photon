f462bc828c3c39a6e6cd10b71eb5c822737e07d3
bai <nkbai@163.com>
2018-10-30 10:46:48 +0800

    update build system

---
+++ cmd/photon/buildall.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+export CGO_ENABLED=0
+export GOOS=linux
+export GOARCH=arm
+export GOARM=5
+export VERSION=0.91
+go env
+./build.sh
+mv photon photon_linux_arm_$VERSION
+zip -r photon_linux_arm_$VERSION.zip photon_linux_arm_$VERSION
+rm -f photon_linux_arm_$VERSION
+export GOOS=linux
+export GOARCH=amd64
+./build.sh
+mv photon photon_linux_amd64_$VERSION
+zip -r photon_linux_amd64_$VERSION.zip photon_linux_amd64_$VERSION
+rm -f photon_linux_amd64_$VERSION
+export GOOS=windows
+export GOARCH=amd64
+./build.sh
+mv photon.exe photon_windows_amd64_$VERSION.exe
+zip -r photon_windows_amd64_$VERSION.zip photon_windows_amd64_$VERSION.exe
+rm -f photon_windows_amd64_$VERSION.exe
+export GOOS=darwin
+export GOARCH=amd64
+./build.sh
+mv photon photon_darwin_amd64_$VERSION
+zip -r photon_darwin_amd64_$VERSION.zip photon_darwin_amd64_$VERSION
+rm -f photon_darwin_amd64_$VERSION
+
