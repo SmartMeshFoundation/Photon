7942308467f39909db26a3d1c64eb50c2502e207
Chuck52 <wuhan_53@163.com>
2018-11-07 18:22:10 +0800

    add pfs test case in casemanager

--- cmd/tools/casemanager/models/TestEnv.go
+++ cmd/tools/casemanager/models/TestEnv.go
@@ -112,12 +112,17 @@ func NewTestEnv(configFilePath string) (env *TestEnv, err error) {
 }
 
 func loadTokenNetworkContract(c *config.Config, conn *ethclient.Client, key *ecdsa.PrivateKey) (registryAddress common.Address, registry *contracts.TokenNetworkRegistry) {
-	addr := c.RdString("COMMON", "token_network_address", "new")
+	addr := c.RdString("COMMON", "registry_contract_address", "new")
 	if addr == "new" {
 		registryAddress, registry = deployRegistryContract(conn, key)
 		Logger.Printf("New RegistryAddress : %s\n", registryAddress.String())
 	} else {
+		var err error
 		registryAddress = common.HexToAddress(addr)
+		registry, err = contracts.NewTokenNetworkRegistry(registryAddress, conn)
+		if err != nil {
+			panic(err)
+		}
 	}
 	Logger.Println("Load RegistryAddress SUCCESS")
 	return
