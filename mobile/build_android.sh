496f63b073ca87877329737064cd386818d06435
bai <nkbai@163.com>
2019-01-15 14:55:27 +0800

    prepare verion 1.0.0

--- mobile/build_android.sh
+++ mobile/build_android.sh
@@ -2,7 +2,7 @@
 export GIT_COMMIT=$(git rev-list -1 HEAD)
 export GO_VERSION=$(go version|sed 's/ //g')
 export BUILD_DATE=$(date|sed 's/ //g'|sed 's/://g')
-export VERSION=0.9.2
+export VERSION=1.0.0
 echo $GIT_COMMIT
 echo $GO_VERSION
 echo $BUILD_DATE
