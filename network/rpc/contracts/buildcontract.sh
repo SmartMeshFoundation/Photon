5a369cee96ddd7b94320bd211cc918d32574385b
bai <nkbai@163.com>
2019-02-22 10:33:28 +0800

    add smt token

--- network/rpc/contracts/buildcontract.sh
+++ network/rpc/contracts/buildcontract.sh
@@ -5,3 +5,4 @@ abigen -solc ./solc2 --sol test/tokens/CustomToken.sol --pkg tokencustom --out t
 abigen -solc ./solc2 --sol test/tokens/HumanERC223Token.sol --pkg tokenerc223 --out test/tokens/tokenerc223/HumanERC223Token.go
 abigen -solc ./solc2 --sol test/tokens/HumanERC223ApproveToken.sol --pkg tokenerc223approve --out test/tokens/tokenerc223approve/HumanERC223ApproveToken.go
 abigen -solc ./solc2 --sol test/tokens/HumanEtherToken.sol --pkg tokenether --out test/tokens/tokenether/HumanEtherToken.go
+abigen -solc ./solc2 --sol test/tokens/SMTToken.sol --pkg smttoken --out test/tokens/smttoken/SMTToken.go
