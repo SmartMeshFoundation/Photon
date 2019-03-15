22153240bf5d551b5092154369e53f6317f8910d
Chuck52 <wuhan_53@163.com>
2019-03-15 09:53:55 +0800

    add lock for HandlePeerFound

--- network/transport.go
+++ network/transport.go
@@ -312,8 +312,8 @@ func (ut *UDPTransport) HandlePeerFound(id string, addr *net.UDPAddr) {
 	if ut != nil {
 		return
 	}
-	ut.lock.RLock()
-	defer ut.lock.RUnlock()
+	ut.lock.Lock()
+	defer ut.lock.Unlock()
 	idFound := common.HexToAddress(id)
 	alreadyFound := false
 	// 清除过期数据,即标志下线
