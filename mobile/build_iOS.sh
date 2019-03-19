78aa604a3e0a98965630fb50fd0d7f079f0dc8ee
bai <nkbai@163.com>
2019-03-19 18:35:17 +0800

    update build script

--- mobile/build_iOS.sh
+++ mobile/build_iOS.sh
@@ -1,8 +1,5 @@
 #!/bin/sh
-export GIT_COMMIT=$(git rev-list -1 HEAD)
-export GO_VERSION=$(go version|sed 's/ //g')
-export BUILD_DATE=`date "+%Y-%m-%d-%H:%M:%S"`
-
+source ../env.sh 
 echo $GIT_COMMIT
 echo $GO_VERSION
 echo $BUILD_DATE
