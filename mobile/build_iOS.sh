adacadad09791dc24d36b5bb0b22d1e325e59b7a
bai <nkbai@163.com>
2019-03-19 22:39:03 +0800

    update build script

--- mobile/build_iOS.sh
+++ mobile/build_iOS.sh
@@ -5,5 +5,5 @@ echo $GO_VERSION
 echo $BUILD_DATE
 gomobile bind -ldflags " -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GitCommit=$GIT_COMMIT -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GoVersion=$GO_VERSION -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.BuildDate=$BUILD_DATE -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.Version=$VERSION"  -target=ios
 
-zip -r iOS_$VERSION.zip Mobile.framework
+zip -r -y iOS_$VERSION.zip Mobile.framework
 rm -rf Mobile.framework
