cfe5679fb96fd851e9b915fbb69feed7644a0255
Chuck52 <wuhan_53@163.com>
2019-03-25 16:56:41 +0800

    upgrade version

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export GIT_COMMIT=$(git rev-list -1 HEAD)
 export GO_VERSION=$(go version|sed 's/ //g')
 export BUILD_DATE=`date "+%Y-%m-%d-%H:%M:%S"`
-export VERSION=1.1.0-${GIT_COMMIT:0-40:4}
+export VERSION=1.1.1-${GIT_COMMIT:0-40:4}
