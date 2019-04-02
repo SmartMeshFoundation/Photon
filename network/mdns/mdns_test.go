7a673ac8ad0e7277c728347196c20498b7db9e6a
bai <nkbai@163.com>
2019-04-02 12:11:38 +0800

    mdns: use best match ip

--- network/mdns/mdns_test.go
+++ network/mdns/mdns_test.go
@@ -8,6 +8,8 @@ import (
 	"testing"
 	"time"
 
+	"github.com/stretchr/testify/assert"
+
 	"github.com/SmartMeshFoundation/Photon/params"
 
 	"github.com/stretchr/testify/require"
@@ -56,4 +58,25 @@ func TestMdnsDiscovery(t *testing.T) {
 	req := require.New(t)
 	//req.Len(n.m, 1, "found b ")
 	req.NotNil(n.m["imb"], "found b ")
+}
+
+func TestGetBestIP(t *testing.T) {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	sa, err := NewMdnsService(ctx, 3000, "ima", time.Second)
+	if err != nil {
+		t.Fatal(err)
+	}
+	m := sa.(*mdnsService)
+	m.service.IPs = []net.IP{
+		net.ParseIP("10.0.0.17").To4(),
+		net.ParseIP("192.168.124.13").To4(),
+	}
+	remotes := []net.IP{
+		net.ParseIP("192.168.124.11").To4(),
+		net.ParseIP("192.168.122.1").To4(),
+	}
+	ip := m.getBestMatchIP(remotes)
+	assert.EqualValues(t, ip.String(), net.ParseIP("192.168.124.11").String())
 }
