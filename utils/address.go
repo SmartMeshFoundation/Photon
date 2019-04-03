21e2eb1e637123d5bbf7f55fd679c2a63d46054d
bai <nkbai@163.com>
2019-04-03 17:08:10 +0800

    disable address validation for input

--- utils/address.go
+++ utils/address.go
@@ -7,11 +7,16 @@ import (
 	"github.com/ethereum/go-ethereum/common"
 )
 
+const addressDefaultValidation = false
+
 /*
 HexToAddress convert hex encoded address string to common.Address,
 and verify it is in accordance with EIP55
 */
 func HexToAddress(addr string) (address common.Address, err error) {
+	if !addressDefaultValidation {
+		return common.HexToAddress(addr), nil
+	}
 	address = common.HexToAddress(addr)
 	if address.String() != addr {
 		err = fmt.Errorf("address checksum error expect=%s,got=%s", address.String(), addr)
