83c7550c2c923aec877d0709cab899466abf393d
bai <nkbai@163.com>
2019-04-03 17:10:05 +0800

    version 1.2.0-rc0

--- env.sh
+++ env.sh
@@ -1,4 +1,4 @@
 export GIT_COMMIT=$(git rev-list -1 HEAD)
 export GO_VERSION=$(go version|sed 's/ //g')
 export BUILD_DATE=`date "+%Y-%m-%d-%H:%M:%S"`
-export VERSION=1.1.2-${GIT_COMMIT:0-40:4}
+export VERSION=1.2.0-rc0
