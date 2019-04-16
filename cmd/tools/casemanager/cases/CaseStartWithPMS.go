cf122601056b72a7188b39e62a850516a4566a41
Chuck52 <wuhan_53@163.com>
2019-04-16 19:26:13 +0800

    casemanager

--- cmd/tools/casemanager/cases/CaseStartWithPMS.go
+++ cmd/tools/casemanager/cases/CaseStartWithPMS.go
@@ -2,6 +2,7 @@ package cases
 
 import (
 	"github.com/SmartMeshFoundation/Photon/cmd/tools/casemanager/models"
+	"github.com/SmartMeshFoundation/Photon/utils"
 )
 
 // CaseStartWithPMS :
@@ -16,13 +17,21 @@ func (cm *CaseManager) CaseStartWithPMS() (err error) {
 		}
 	}()
 	// 源数据
-	//tokenAddress := env.Tokens[0].TokenAddress.String()
+	tokenAddress := env.Tokens[0].TokenAddress.String()
 	N0, N1 := env.Nodes[0], env.Nodes[1]
 	models.Logger.Println(env.CaseName + " BEGIN ====>")
 	// 启动pms
 	env.StartPMS()
 	// 启动节点0,1
 	cm.startNodesWithPMS(env, N0, N1)
+	err = N0.OpenChannel(utils.NewRandomAddress().String(), tokenAddress, 1, 58)
+	if err == nil {
+		return cm.caseFail(env.CaseName)
+	}
+	err = N0.OpenChannel(utils.NewRandomAddress().String(), tokenAddress, 1, -1)
+	if err != nil {
+		return cm.caseFail(env.CaseName)
+	}
 	//cm.waitForPostman()
 	models.Logger.Println(env.CaseName + " END ====> SUCCESS")
 	return nil
