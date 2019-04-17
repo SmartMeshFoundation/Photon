212c551aed64856cd8a338a972e30cb39eb78402
bai <nkbai@163.com>
2019-04-17 17:22:52 +0800

    mobile: remove useless cpu arch

--- mobile/build_iOS.sh
+++ mobile/build_iOS.sh
@@ -3,7 +3,7 @@ source ../env.sh
 echo $GIT_COMMIT
 echo $GO_VERSION
 echo $BUILD_DATE
-gomobile bind -ldflags " -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GitCommit=$GIT_COMMIT -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GoVersion=$GO_VERSION -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.BuildDate=$BUILD_DATE -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.Version=$VERSION"  -target=ios
+gomobile bind  -v -ldflags " -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GitCommit=$GIT_COMMIT -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GoVersion=$GO_VERSION -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.BuildDate=$BUILD_DATE -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.Version=$VERSION"  -target=ios/arm64,ios/arm
 
 zip -r -y iOS_$VERSION.zip Mobile.framework
 rm -rf Mobile.framework
