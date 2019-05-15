cb1241e1e8cdee4bef38daa782f7afbf455cce55
bai <nkbai@163.com>
2019-05-15 15:51:48 +0800

    mediatedtransfer: transfer amount should include all fee

--- transfer/mediatedtransfer/initiator/transition.go
+++ transfer/mediatedtransfer/initiator/transition.go
@@ -74,8 +74,10 @@ func tryNewRoute(state *mt.InitiatorState) *transfer.TransitionResult {
 	for len(state.Routes.AvailableRoutes) > 0 {
 		r := state.Routes.AvailableRoutes[0]
 		state.Routes.AvailableRoutes = state.Routes.AvailableRoutes[1:]
-		//if !r.CanTransfer() /*交易发起方不应该考虑收费*/ || r.AvailableBalance().Cmp(new(big.Int).Add(state.Transfer.TargetAmount, r.Fee)) < 0 {
-		if !r.CanTransfer() || r.AvailableBalance().Cmp(state.Transfer.TargetAmount) < 0 {
+		/*
+			发起方计算的时候应该把整条路径的费用考虑进去,也就是下面新创建的LockedTransferState中的Amount
+		*/
+		if !r.CanTransfer() || r.AvailableBalance().Cmp(new(big.Int).Add(state.Transfer.TargetAmount, r.TotalFee)) < 0 {
 			state.Routes.IgnoredRoutes = append(state.Routes.IgnoredRoutes, r)
 		} else {
 			tryRoute = r
