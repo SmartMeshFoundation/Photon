bd2af9ce7e42d06a0df5d318f97287db74da0b30
bai <nkbai@163.com>
2019-05-31 10:45:49 +0800

    mdns: add debug log

--- network/mdns/mdns.go
+++ network/mdns/mdns.go
@@ -182,7 +182,7 @@ func (m *mdnsService) getBestMatchIP(remotes []net.IP) net.IP {
 	return remotes[0]
 }
 func (m *mdnsService) handleEntry(e *mdns.ServiceEntry) {
-	//log.Debug(fmt.Sprintf("Handling MDNS entry: %s:%d %s", e.AddrV4, e.Port, e.Info))
+	log.Trace(fmt.Sprintf("Handling MDNS entry: %s:%d %s", e.AddrV4, e.Port, e.Info))
 
 	if e.Info == m.myid {
 		//log.Debug("got our own mdns entry, skipping")
