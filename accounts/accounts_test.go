9c3fa48749ed46bc626401aaef414a7970019056
Chuck52 <wuhan_53@163.com>
2019-07-29 18:23:22 +0800

    fix test code

--- accounts/accounts_test.go
+++ accounts/accounts_test.go
@@ -7,22 +7,10 @@ import (
 	"runtime"
 
 	"github.com/SmartMeshFoundation/Photon/utils"
-	"github.com/ethereum/go-ethereum/crypto"
 )
 
 func TestDefaultKeyStoreDir(t *testing.T) {
 	t.Log(utils.DefaultKeyStoreDir())
-}
-
-func TestAccountManager(t *testing.T) {
-	am := NewAccountManager("/home/chuck/code/run/smc/testnet/keystore")
-	privkey, err := am.GetPrivateKey(am.Accounts[2].Address, "123")
-	if err != nil {
-		t.Error(err)
-	}
-	t.Logf("privkey=0x%s", hex.EncodeToString(privkey))
-	p, _ := crypto.ToECDSA(privkey)
-	t.Logf("pubKey=0x%s", crypto.PubkeyToAddress(p.PublicKey).String())
 }
 
 func TestFininalize(t *testing.T) {
