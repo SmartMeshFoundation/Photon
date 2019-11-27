c46e43a593d5ac3283fb23fd5f13c96f10d615ff
Chuck52 <wuhan_53@163.com>
2019-11-27 11:37:42 +0800

    VERSION 1.2.1

--- cmd/photon/build.sh
+++ cmd/photon/build.sh
@@ -7,4 +7,4 @@ echo $GIT_COMMIT
 
 go  build  -ldflags "   -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GitCommit=$GIT_COMMIT -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.GoVersion=$GO_VERSION -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.BuildDate=$BUILD_DATE -X github.com/SmartMeshFoundation/Photon/cmd/photon/mainimpl.Version=$VERSION "
 
-cp photon $GOPATH/bin
+cp photon $GOBIN/
